bdf889fd4dbd970e8bbcf0aa882eafb7daaafb80
Viktor Malieichyk <mwChief@gmail.com>
2021-05-12 14:49:14 +0300

    added segment analytics

--- analytics_warning/ViewController.swift
+++ analytics_warning/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import Analytics
+//import Segment
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        SEGAnalytics.setup(with: SEGAnalyticsConfiguration(writeKey: "jfgsdhgk"))
+//        let configuration = AnalyticsConfiguration(writeKey: "token")
+//        configuration.trackApplicationLifecycleEvents = true
+//        configuration.trackDeepLinks = true
+//        configuration.trackPushNotifications = true
+//        Analytics.setup(with: configuration)
     }
 
 
